2154dd4313c9d8af9ef5f08b2896953e1ecfa84f
Christian Bongard <christian.bongard90@gmail.com>
2021-09-03 21:44:03 +0400

    testing the controllers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,8 @@ class HomeController < ApplicationController
   def index
   end
   def about
+    @about_me ="My name is Christian Bongard" #instance variable
+    about_me ="My name is Christian Bongard" #local variable, will only work within this file
+    @answer = 2+2
   end
 end
