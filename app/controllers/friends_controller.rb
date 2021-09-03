82b49f3dabdcee3259bd50a407e0cc7b0669202d
Christian Bongard <christian.bongard90@gmail.com>
2021-09-03 16:39:44 +0400

    Association of friends to users

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -64,6 +64,6 @@ class FriendsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def friend_params
-      params.require(:friend).permit(:first_name, :last_name, :phone, :email, :twitter)
+      params.require(:friend).permit(:first_name, :last_name, :phone, :email, :twitter,:user_id)
     end
 end
